3c52e306e74a5d0b085b60c5b57decba8b755f31
yourfavengineer <kendall8584@yahoo.com>
2025-05-19 18:57:08 -0700

    run production images

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -156,3 +157,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
+
+from .cdn.conf import * 
